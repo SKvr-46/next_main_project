0cc8f3d616350395941c5c008510e6bc4f47dfe2
SKvr-46 <koeich4@gmail.com>
2023-02-20 21:04:20 +0900

    commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,12 +12,12 @@ const About:NextPage = () => {
     const background_2 ="クラーク記念国際高等学校 卒"
     const background_3 = "明治大学 総合数理学部 卒 （位相幾何学）"
     const background_4 = "東京大学大学院 地球惑星科学専攻 （地震学）"
-    const skills_1 = "-----プログラミング言語-----"
+    const skills_1 = "-----使用言語など-----"
     const skills_2 = "研究： Python Fortran C"
     const skills_3 = "WEB： HTML CSS Javascript Typescript React/Next.js"
     const skills_4 = "その他の言語(触ったことがある程度)： PHP Ruby Java"
     const personal_1 = "-----趣味-----"
-    const personal_2 = "趣味：チェス、数学、トレーニング"
+    const personal_2 = "チェス、数学、トレーニング"
     return (
         <>
             <Meta pageTitle={"About Me"} pageDesc={"研究部屋αの管理人のページ"}/>
