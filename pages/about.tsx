b0b89a79ec3389cffb84b88a9f78c5931aaac37d
SKvr-46 <koeich4@gmail.com>
2023-02-20 20:47:07 +0900

    commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,13 +8,16 @@ import { GetStaticProps, NextPage } from "next";
 
 
 const About:NextPage = () => {
-    const background_1 ="クラーク記念国際高等学校 卒"
-    const background_2 = "明治大学 総合数理学部 卒 （位相幾何学）"
-    const background_3 = "東京大学大学院 地球惑星科学専攻 （地震学）"
-    const skills_1 = "研究： Python Fortran C"
-    const skills_2 = "WEB： HTML CSS Javascript Typescript React/Next.js"
-    const skills_3 = "その他の言語(触ったことがある程度)： PHP Ruby Java"
-    const personal = "趣味：チェス、数学、トレーニング"
+    const background_1 ="-----学歴-----"
+    const background_2 ="クラーク記念国際高等学校 卒"
+    const background_3 = "明治大学 総合数理学部 卒 （位相幾何学）"
+    const background_4 = "東京大学大学院 地球惑星科学専攻 （地震学）"
+    const skills_1 = "-----プログラミング言語-----"
+    const skills_2 = "研究： Python Fortran C"
+    const skills_3 = "WEB： HTML CSS Javascript Typescript React/Next.js"
+    const skills_4 = "その他の言語(触ったことがある程度)： PHP Ruby Java"
+    const personal_1 = "-----趣味-----"
+    const personal_2 = "趣味：チェス、数学、トレーニング"
     return (
         <>
             <Meta pageTitle={"About Me"} pageDesc={"研究部屋αの管理人のページ"}/>
@@ -32,6 +35,7 @@ const About:NextPage = () => {
                 subtitle_1={background_1} 
                 subtitle_2={background_2}
                 subtitle_3={background_3}
+                subtitle_4={background_4}
                 backgroundcolor={"black"} 
                 color={"white"}/>
                 <Card 
@@ -39,11 +43,13 @@ const About:NextPage = () => {
                 subtitle_1={skills_1}
                 subtitle_2={skills_2}
                 subtitle_3={skills_3}
+                subtitle_4={skills_4}
                 backgroundcolor={"purple"} 
                 color={"white"}/>
                 <Card 
                 title={"Personal"} 
-                subtitle_1={personal} 
+                subtitle_1={personal_1} 
+                subtitle_2={personal_2} 
                 backgroundcolor={"#20b2aa"} 
                 color={"black"}/>
             </Container>
