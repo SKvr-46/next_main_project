69f5e6757773bfb5fa53e2c0c273b835d9d7e50e
SKvr-46 <koeich4@gmail.com>
2023-04-08 18:46:50 +0900

    update meta

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const siteMeta = {
     siteTitle: "研究部屋β",
     siteDesc: "地震学の研究内容を紹介するWEBページ",
-    siteUrl: "https://kenkyuubeyaalpha",
+    siteUrl: "https://myproject-skvr-46.vercel.app",
     siteLang: "ja",
     siteLocale: "ja-JP",
     siteType: "website",
